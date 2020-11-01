4b3d6a45a0118c821fb763f42c091f31c3eb47f5
Fernando <yahirokendy@gmail.com>
2020-11-01 11:57:36 -0400

    Subindo delete, edit e a index(arrumada)

--- src/Controller/AlunosController.php
+++ src/Controller/AlunosController.php
@@ -20,59 +20,46 @@ class AlunosController extends AppController
      */
     public function index($id = null)
     {   
-        $this->autoRender = false;
+        $response = $this->response->withType('application/json');
         
         $alunos = $this->Alunos->find();
-        $json = array();
 
-        try{
+        
+        if (!empty($id)) {
+            $statusCode = 404;               //CONFERIR ANTES DE SUBIR
+            $json = "Não foi possível encontrar usuário :(";
+
             $alunos->where(['Alunos.id' => $id])->first();
-            if(empty($alunos)){
-                throw new Exception('Usuário não foi encontrado.');
-            }
-            else{
-                if(!empty($this->request->getQuery('nome'))){
-                    $alunos->andWhere(['Alunos.nome' => $this->request->getQuery('nome')]);
-                }
-                if(!empty($this->request->getQuery('pagina'))){
-                    // $alunos->limit($this->request->getQuery('pagina'));
-
-                    //Deve usar aquela paradinha de paginação
-                }
-
-                // O isset estava dando erro kkkk
-                if($this->request->getQuery('limite') >= 0){
-                    $alunos->limit($this->request->getQuery('limite'));
-                }
-                $this->response->statusCode(200);
-                $json = $alunos;
-                
-            }
-
-        }catch(Exception $e){
-            $json = [
-                'msg' => 'Não foi encontrado aluno com essas informações :(',
-            ];
-            $this->response->statusCode(400);
+            
+            
         }
-      
-        return $this->response->withType('json')->withStringBody(json_encode($json));
-    }
+        else{
+            $statusCode = 400;               //CONFERIR ANTES DE SUBIR
+            $json = "Não foi possível encontrar com esses parâmetros!.";
 
-    /**
-     * View method
-     *
-     * @param string|null $id Aluno id.
-     * @return \Cake\Http\Response|null|void Renders view
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function view($id = null)
-    {
-        $aluno = $this->Alunos->get($id, [
-            'contain' => ['SituacaoCadastros'],
-        ]);
+            $alunos->where(['Alunos.nome !=' =>  '']);
+            if(!empty($this->request->getQuery('nome'))){
+                $alunos->andWhere(['Alunos.nome' => $this->request->getQuery('nome')]);
+            }
 
-        $this->set(compact('aluno'));
+            if($this->request->getQuery('limite') >= 0){
+                $alunos->limit($this->request->getQuery('limite'));
+            }
+
+            // Ver como fazer a paginação com o Mateus
+
+            // if(!empty($this->request->getyQuery('pagina'))){
+            //     $alunos->paginate([$this->request->getyQuery('pagina')]);
+            // }
+
+        }
+        if (!empty($alunos)) {
+            $statusCode = 200;
+            $json = $alunos;
+        }
+
+        $response = $response->withStatus($statusCode);
+        return $response->withStringBody(json_encode($json));
     }
 
     /**
@@ -117,22 +104,32 @@ class AlunosController extends AppController
      */
     public function edit($id = null)
     {
-        $aluno = $this->Alunos->get($id, [
-            'contain' => [],
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $aluno = $this->Alunos->patchEntity($aluno, $this->request->getData());
-            if ($this->Alunos->save($aluno)) {
-                $this->Flash->success(__('The aluno has been saved.'));
+        $response = $this->response->withType('application/json');
 
-                return $this->redirect(['action' => 'index']);
+        $statusCode = 405;                   //CONFERIR ANTES DE SUBIR
+        $json = "ID Inválido.";
+
+        if (!empty($id)) {
+            $statusCode = 400;               //CONFERIR ANTES DE SUBIR
+            $json = "Não foi possível modificar :(";
+            $aluno = $this->Alunos->get($id, [
+                'contain' => [],
+            ]);
+
+            if ($this->request->is(['patch', 'post', 'put'])) {
+                if (!empty($aluno)) {
+                    $statusCode = 200;
+                    $json = "Aluno deletado com sucesso!";
+
+                    $aluno = $this->Alunos->patchEntity($aluno, $this->request->getData());
+                    $this->Alunos->save($aluno);
+                }
             }
-            $this->Flash->error(__('The aluno could not be saved. Please, try again.'));
         }
-        $situacaoCadastros = $this->Alunos->SituacaoCadastros->find('list', ['limit' => 200]);
-        $this->set(compact('aluno', 'situacaoCadastros'));
+        $response = $response->withStatus($statusCode);
+        return $response->withStringBody(json_encode($json));
     }
-
+    
     /**
      * Delete method
      *
@@ -142,22 +139,25 @@ class AlunosController extends AppController
      */
     public function delete($id = null)
     {
-        // $this->request->allowMethod(['post', 'delete']);
-        $aluno = $this->Alunos->find()->where(['Alunos.id' => $id])->first();
-
-        try{
-            if ($this->Alunos->delete($aluno)) {
-                $this->Flash->success(__('The aluno has been deleted.'));
-            } else {
-                throw new Exception('The aluno could not be deleted. Please, try again.');
-            }
-        }catch(Exception $e){
-            $json = [
-                ''
-            ];
-        }
+        $this->request->allowMethod(['post', 'delete']);
+        $response = $this->response->withType('application/json');
         
+        $statusCode = 405;                   //CONFERIR ANTES DE SUBIR
+        $json = "ID Inválido.";
 
-        return $this->redirect(['action' => 'index']);
+        if (!empty($id)) {
+            $statusCode = 400;               //CONFERIR ANTES DE SUBIR
+            $json = "Não foi possível excluir :(";
+
+            $aluno = $this->Alunos->find()->where(['Alunos.id' => $id])->first();
+            if (!empty($aluno)) {
+                $this->Alunos->delete($aluno);
+                $statusCode = 200;
+                $json = "Aluno deletado com sucesso!";
+            }
+        }
+        $response = $response->withStatus($statusCode);
+        return $response->withStringBody(json_encode($json));
+      
     }
 }
